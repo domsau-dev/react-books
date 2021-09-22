3eb81dee315b23fb0dfd8632929a23272ac25625
domsau-dev <84323419+domsau-dev@users.noreply.github.com>
2021-09-22 22:51:45 +0300

    Button update

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,12 +1,14 @@
 function Book(props) {
 
+    const hide = () => document.querySelector(".book" + props.data.id).style.display = "none";
+
     return (
-        <div className="book">
+        <div className={"book book" + props.data.id}>
             <img src={props.data.img} alt="" />
             <div className="author">{props.data.author}</div>
             <div className="title">{props.data.title}</div>
             <div className="price">Kaina: {props.data.price} €</div>
-            <button>Pirkti</button>
+            <button onClick={hide}>Pirkti</button>
         </div>
     );
 }
